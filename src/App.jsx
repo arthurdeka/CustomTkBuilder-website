d98be864fac9f5435cdb138e099d6c0e67a8b71a
Arthur <arthurdk01@gmail.com>
2024-07-23 23:06:59 -0300

    feature/ga4
    google analytics implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Header from "./components/Header";
 import Canvas from "./canvas/canvas";
+import ReactGA from 'react-ga4';
 import { Sidemenu } from "./components/Sidemenu";
 import { CanvasProvider } from "./canvas/CanvasContext";
 import { Properties } from "./components/Properties";
+import { useEffect } from "react";
 
 function App() {
+  ReactGA.initialize("G-N6T5V13HJ4");
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Home" });
+  }, []);
 
   return (
     <div className="App bg-gray-900 max-h-screen overflow-hidden ">
