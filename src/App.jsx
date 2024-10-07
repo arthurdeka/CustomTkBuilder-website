857696efdad8356b085999dd115e2f3d9d3c65cd
Arthur <arthurdk01@gmail.com>
2024-10-07 19:27:06 -0300

    bugfix/router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Canvas from "./canvas/canvas";
 import ReactGA from 'react-ga4';
